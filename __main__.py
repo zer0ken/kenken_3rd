78149c55d916aeabc025c6eb5aa4e94b58c3a287
zer0ken <a37592777@gmail.com>
2023-02-22 01:50:40 +0900

    answer!

--- __main__.py
+++ __main__.py
@@ -8,9 +8,6 @@ import random
 
 from boto.s3.connection import S3Connection
 
-
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
-
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='?', intents=intents)
 
@@ -49,6 +46,8 @@ async def on_ready():
 async def on_name_called(message):
     if message.author == bot.user:
         return
+    if '켄켄' not in message.content and bot.user not in message.mentions:
+        return 
     answer = [
         '불럿어?',
         '불ㄹ엇어?',
@@ -73,5 +72,6 @@ async def on_emoji_message(message):
         await message.channel.send(embed=embeds[0], reference=message, view=view, delete_after=30)
 
 
-bot_token = os.environ["BOT_TOKEN"]
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+bot_token = os.environ['BOT_TOKEN']
 bot.run(bot_token)
